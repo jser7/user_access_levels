1fa6573ebfe88af881d99685945511bab44051c2
jser7 <116626521+jser7@users.noreply.github.com>
2022-11-02 21:21:16 +0000

    Update userAccessLevels.py

--- userAccessLevels.py
+++ userAccessLevels.py
@@ -1,45 +1,38 @@
 # User Access Levels
 # Get data from JSON file for passwords
-# Import libraries, set a fernet key.
-import cryptography
-from cryptography.fernet import Fernet
 import json
-k = Fernet.generate_key()
-fernet = Fernet(k)
+import hashlib
 
 
-# Open JSON file, and encrypt the json dictionary.
+# Open JSON file
 with open("json_dict.json", "r") as f:
     data = json.load(f)
-encrypt_data = fernet.encrypt(str(data).encode())
 
 
-print("Welcome to the system. Please input your name.")
 name = input()
 # User inputs their name, if their name is recognised in the dictionary then they have to
 # Enter a password and they will get superior access
-name_level_dict = {"Jaden": 3, "Leah": 2, "Guest": 1}
+name_level_dict = {"Jaden": 3, "Violet": 2, "Guest": 1}
 
 
 def password(level):
     # User enters their password
+    # Password is hashed, if it matches the hash found in the JSON file, the password is correct.
     print("Password required. Please input the  password.")
-    passAttempt = input()
-    decrypted = fernet.decrypt(encrypt_data).decode()
-    # Decrypt the previously encrypted JSON dictionary, and turn it back into a dictionary
-    json_acceptable_string = decrypted.replace("'", '"')
-    d = json.loads(json_acceptable_string)
+    pass_inp = input()
+    hashed_pass = hashlib.sha512(pass_inp.encode())
+    hash_pass_digest = hashed_pass.hexdigest()
 
-    # If the user's password attempt is equal to the password found in the json file, then give access
-    if passAttempt == d["passwords"][str(level)]:
-        print("valid access")
+    # Check password matches JSON file password
+    if hash_pass_digest == data["passwords"][str(level)]:
+        print("Access Granted")
     else:
-        print("invalid access")
+        print("Access Denied")
 
 
 def checkLevel(dict_name):
-    # dictName is the name the user inputted at the start.
-    # If dictName is found in the list of users in the system, then check their access level.
+    # dict_name is the name the user inputted at the start.
+    # If dict_name is found in the list of users in the system, then check their access level.
     if dict_name in name_level_dict:
         if name_level_dict[dict_name] > 1:
             password(name_level_dict[dict_name])
